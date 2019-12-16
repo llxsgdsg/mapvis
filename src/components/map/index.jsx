8abab9e1a02514e1855766c1d3aa2f7b99c28210
llxsgdsg <550304366@qq.com>
2019-12-16 10:58:09 +0800

    first commit

---
+++ src/components/map/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './map.css'
+
+export default class Map extends Component {
+    
+    render(){
+        return <div id='map' >hhh</div>
+    }
+    
+}
